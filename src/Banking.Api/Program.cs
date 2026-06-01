927ed1fafe7628da084c61e21422bf831a5baab4
Flavio Portugal <flavio.conceicao93@gmail.com>
2026-06-01 12:11:57 -0300

    Adiciona Swagger

--- src/Banking.Api/Program.cs
+++ src/Banking.Api/Program.cs
@@ -3,12 +3,16 @@ using Banking.Application.Services;
 using Banking.Domain.Interfaces;
 using Banking.Infrastructure.Messaging;
 using Banking.Infrastructure.Repositories;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Banking API", Version = "v1" });
+});
 
 builder.Services.AddScoped<PixService>();
 builder.Services.AddScoped<IContaRepository, ContaRepositoryFake>();
@@ -16,8 +20,27 @@ builder.Services.AddScoped<IMessagePublisher, RabbitMqPublisher>();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking API v1");
+        c.RoutePrefix = string.Empty; // serve Swagger UI at application's root
+    });
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
 
 app.MapControllers();
 
